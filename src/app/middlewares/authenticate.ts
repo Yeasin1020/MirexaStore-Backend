99846fbdbe075ee82fd444554127c4446ce147c4
Yeasin1st <ysbd111@gmail.com>
2024-08-28 02:36:02 +0600

    some error solve

--- src/app/middlewares/authenticate.ts
+++ src/app/middlewares/authenticate.ts
@@ -9,16 +9,18 @@ const authenticate = async (req: Request, res: Response, next: NextFunction) => 
 		if (!token) {
 			return res.status(401).json({ message: 'No token provided' });
 		}
-
 		// eslint-disable-next-line @typescript-eslint/no-explicit-any
-		const decoded: any = jwt.verify(token, config.jwt_access_secret as string);
+		const decoded = jwt.verify(token, config.jwt_access_secret as string) as any;
 
-		const user = await User.findById(decoded._id).select('-password');
+		console.log('Decoded token:', decoded);
+
+		const user = await User.findOne({ email: decoded.email }).select('-password');
+
+
 		if (!user) {
 			return res.status(401).json({ message: 'Invalid token' });
 		}
-
-		req.user = user; // Attach user to the request object
+		req.user = user;
 		next();
 	} catch (err) {
 		return res.status(401).json({ message: 'Unauthorized' });
